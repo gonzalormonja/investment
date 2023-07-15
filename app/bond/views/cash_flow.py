f83ac60afe31800ba7f7e17972fc21a2038eac63
Gonzalo Monja <gonzalormonja1@gmail.com>
2023-07-15 03:58:11 +0000

    feat: update cash flow scrapping

--- app/bond/views/cash_flow.py
+++ app/bond/views/cash_flow.py
@@ -97,6 +97,7 @@ class CashFlowViewSet(generics.ListAPIView, viewsets.GenericViewSet):
     def list(self, request, *args, **kwargs):
         bond_names = self.request.query_params.get("bond_names").split(",")
         response = []
+        bond_status_responses = []
         for bond_name in bond_names:
             driver = webdriver.Chrome(options=self.set_chrome_options())
 
@@ -135,7 +136,9 @@ class CashFlowViewSet(generics.ListAPIView, viewsets.GenericViewSet):
                     bad_gateway = True if "Bad Gateway" in driver.page_source else False
                     tries += 1
                 if bad_gateway is True:
-                    return Response("Bad gateway")
+                    bond_status_responses.append(
+                        {"bond_name": bond_name, "status": "bad gateway"}
+                    )
 
                 login_button = self._get_element(
                     driver,
@@ -155,116 +158,86 @@ class CashFlowViewSet(generics.ListAPIView, viewsets.GenericViewSet):
                     login_button.click()
                     sleep(10)
 
-                current_price = self.sanitize_decimal(
-                    self._get_element_text(
+                bad_gateway = True
+                tries = 0
+                while bad_gateway and tries <= 10:
+                    loaded = self._get_element(
                         driver,
-                        "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[3]/div/strong",
+                        "/html/body/div[1]/div[3]/div/div/div/div[1]/div/h2/strong/strong",
+                        False,
                     )
-                )
-                current_tir = self.sanitize_decimal(
-                    self._get_element_text(
-                        driver,
-                        "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div/strong",
+                    tries += 1
+                    if loaded is not None:
+                        bad_gateway = False
+                    else:
+                        sleep(10)
+                if bad_gateway is True:
+                    bond_status_responses.append(
+                        {"bond_name": bond_name, "status": "loading"}
                     )
-                )
-                current_duration = self.sanitize_decimal(
-                    self._get_element_text(
-                        driver,
-                        "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[2]/div/strong",
-                    )
-                )
-                current_parity = self.sanitize_decimal(
-                    self._get_element_text(
-                        driver,
-                        "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[4]/div/strong",
-                    )
-                )
-                currency_code = self._get_element_text(
-                    driver,
-                    "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[3]/div/span[2]/span[1]",
-                )
-
-                if bond is not None:
-                    if cash_flows is not None and len(cash_flows) > 0:
-                        processed_cash_flow = map(
-                            lambda x: {
-                                "date": x.date,
-                                "interest": x.interest,
-                                "percentage": "{:.2f}%".format(
-                                    (float(x.interest) / float(current_price)) * 100
-                                ),
-                                "bond_name": bond_name,
-                            },
-                            cash_flows,
-                        )
-                        response = response + list(processed_cash_flow)
-
-                if bond is None:
-                    bond_type = self._get_element_text(
-                        driver,
-                        "/html/body/div[1]/div[3]/div/div/div/div[1]/div/h2/strong/small",
-                    )
-                    bond = Bond.objects.create(
-                        name=bond_name,
-                        type="corporative"
-                        if "Obligación Negociable" in bond_type
-                        else "national",
-                        last_scrap_date=datetime.now(),
-                        last_scrap_price=current_price,
-                        last_scrap_tir=current_tir,
-                        last_scrap_duration=current_duration,
-                        last_scrap_parity=current_parity,
-                        currency_code=currency_code,
-                    )
-                if len(cash_flows) <= 0:
-                    payments = self._get_elements(
-                        driver,
-                        "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div/div[2]/div/div[2]/table/tbody/tr",
-                    )
-                    for payment in payments:
-                        date = payment.find_element(By.XPATH, "td[2]").text
-                        interest = (
-                            payment.find_element(By.XPATH, "td[3]")
-                            .text.replace(".", "")
-                            .replace(",", ".")
-                        )
-                        percentage = "{:.2f}%".format(
-                            (float(interest) / float(current_price)) * 100
-                        )
-                        amortization = (
-                            payment.find_element(By.XPATH, "td[4]")
-                            .text.replace(".", "")
-                            .replace(",", ".")
-                        )
-                        CashFlow.objects.create(
-                            bond=bond,
-                            date=datetime.strptime(date, "%d/%m/%Y"),
-                            interest=interest,
-                            amortization=amortization,
-                        )
-                        response.append(
-                            {
-                                "date": date,
-                                "interest": interest,
-                                "percentage": percentage,
-                                "bond_name": bond_name,
-                            }
-                        )
-                    next_page_button = None
-                    try:
-                        next_page_button = self._get_element(
+                else:
+                    current_price = self.sanitize_decimal(
+                        self._get_element_text(
                             driver,
-                            "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div/div[2]/div/div[3]/div[2]/div/ul/li[2]/a",
+                            "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[3]/div/strong",
                         )
-                    except NoSuchElementException:
-                        pass
-                    if (
-                        next_page_button is not None
-                        and next_page_button.is_displayed()
-                        and next_page_button.is_enabled()
-                    ):
-                        next_page_button.click()
-                        sleep(2)
+                    )
+                    current_tir = self.sanitize_decimal(
+                        self._get_element_text(
+                            driver,
+                            "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div/strong",
+                        )
+                    )
+                    current_duration = self.sanitize_decimal(
+                        self._get_element_text(
+                            driver,
+                            "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[2]/div/strong",
+                        )
+                    )
+                    current_parity = self.sanitize_decimal(
+                        self._get_element_text(
+                            driver,
+                            "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[4]/div/strong",
+                        )
+                    )
+                    currency_code = self._get_element_text(
+                        driver,
+                        "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[1]/div[3]/div/span[2]/span[1]",
+                    )
+
+                    if bond is not None:
+                        if cash_flows is not None and len(cash_flows) > 0:
+                            processed_cash_flow = map(
+                                lambda x: {
+                                    "date": x.date,
+                                    "interest": x.interest,
+                                    "percentage": "{:.2f}%".format(
+                                        (float(x.interest) / float(current_price)) * 100
+                                    ),
+                                    "bond_name": bond_name,
+                                },
+                                cash_flows,
+                            )
+                            response = response + list(processed_cash_flow)
+
+                    if bond is None:
+                        bond_type = self._get_element_text(
+                            driver,
+                            "/html/body/div[1]/div[3]/div/div/div/div[1]/div/h2/strong/small",
+                        )
+                        bond = Bond.objects.create(
+                            name=bond_name,
+                            type="corporative"
+                            if "Obligación Negociable" in bond_type
+                            else "national",
+                            last_scrap_date=datetime.now(),
+                            last_scrap_price=current_price,
+                            last_scrap_tir=current_tir,
+                            last_scrap_duration=current_duration,
+                            last_scrap_parity=current_parity,
+                            currency_code=currency_code,
+                        )
+                    if len(cash_flows) <= 0:
                         payments = self._get_elements(
                             driver,
                             "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div/div[2]/div/div[2]/table/tbody/tr",
@@ -298,4 +271,54 @@ class CashFlowViewSet(generics.ListAPIView, viewsets.GenericViewSet):
                                     "bond_name": bond_name,
                                 }
                             )
-        return Response(response)
+                        next_page_button = None
+                        try:
+                            next_page_button = self._get_element(
+                                driver,
+                                "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div/div[2]/div/div[3]/div[2]/div/ul/li[2]/a",
+                            )
+                        except NoSuchElementException:
+                            pass
+                        if (
+                            next_page_button is not None
+                            and next_page_button.is_displayed()
+                            and next_page_button.is_enabled()
+                        ):
+                            next_page_button.click()
+                            sleep(2)
+                            payments = self._get_elements(
+                                driver,
+                                "/html/body/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div[3]/div[2]/div/div/div[2]/div/div[2]/table/tbody/tr",
+                            )
+                            for payment in payments:
+                                date = payment.find_element(By.XPATH, "td[2]").text
+                                interest = (
+                                    payment.find_element(By.XPATH, "td[3]")
+                                    .text.replace(".", "")
+                                    .replace(",", ".")
+                                )
+                                percentage = "{:.2f}%".format(
+                                    (float(interest) / float(current_price)) * 100
+                                )
+                                amortization = (
+                                    payment.find_element(By.XPATH, "td[4]")
+                                    .text.replace(".", "")
+                                    .replace(",", ".")
+                                )
+                                CashFlow.objects.create(
+                                    bond=bond,
+                                    date=datetime.strptime(date, "%d/%m/%Y"),
+                                    interest=interest,
+                                    amortization=amortization,
+                                )
+                                response.append(
+                                    {
+                                        "date": date,
+                                        "interest": interest,
+                                        "percentage": percentage,
+                                        "bond_name": bond_name,
+                                    }
+                                )
+            bond_status_responses.append({"bond_name": bond_name, "status": "ok"})
+            driver.quit()
+        return Response(bond_status_responses)
